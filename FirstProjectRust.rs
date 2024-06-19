0bb281631bdf5ea9faecc590fe21a3e1ff5c6734
Kalcha <diego.rbarrionuevo@gmail.com>
2024-06-19 17:34:43 -0300

    first commit

---
+++ FirstProjectRust.rs
@@ -0,0 +1,48 @@
+use anchor_lang::prelude::*;
+
+declare_id!("3ngpiiFDK2aY2PybcUhjJJMs9jsnj5cRXLrVpMfJrig2");
+
+#[program]
+mod programa_contador {
+    use super::*;
+
+    pub fn crear_contador(ctx: Context<Crear>, primer_numero: u64) -> Result<()> {
+        ctx.accounts.contador.numero = primer_numero;
+        ctx.accounts.contador.autoridad = ctx.accounts.autoridad.key();
+        msg!("creando un contador con numero inicial {} ", primer_numero);
+        Ok(())
+    }
+
+    pub fn borrar_contador(_ctx: Context<Borrar>) -> Result<()> {
+        msg!("Contador eliminado");
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Crear<'info> {
+    // 8 bytes para discriminador  + (lo que ocupe tu estructura)
+    #[account(init, payer = autoridad, space = 8 + 8 + 32)]
+    pub contador: Account<'info, Contador>,
+    #[account(mut)]
+    pub autoridad: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Borrar<'info> {
+    #[account(mut)]
+    pub autoridad: Signer<'info>,
+    #[account(
+        mut,
+        constraint = contador.autoridad == contador.key(),
+        close = autoridad
+    )]
+    pub contador: Account<'info, Contador>,
+}
+
+#[account]
+pub struct Contador {
+    numero: u64,       // 8 bytes
+    autoridad: Pubkey, // 32 bytes
+}
